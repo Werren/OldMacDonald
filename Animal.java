479422b79e4d7f04d3ebbaacdfc0df18bf177869
Warren <31322399+Werren@users.noreply.github.com>
2017-10-20 10:34:49 -0700

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,4 +1,12 @@
-interface Animal 
-{    
-	   //your code here
+public void setup() {     
+    Cow c = new Cow("Cow", "moo");   
+    System.out.println(c.getType() + " goes " + c.getSound()); 
+      Chick a = new Chick("Chick","bawk");
+    System.out.println(a.getType() + " goes " + a.getSound());   
+         Pig b = new Pig("Pig","oink");
+    System.out.println(b.getType() + " goes " + b.getSound());   
+}
+interface Animal {    
+  public String getSound();        
+  public String getType(); 
 }  
