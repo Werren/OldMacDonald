800ed4bc8bcc3673664a05e31618a10d9313d84b
Warren <31322399+Werren@users.noreply.github.com>
2017-10-24 02:13:50 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,24 @@
-class NamedCow
+class NamedCow implements Animal
 {
-
+  private String myType;     
+  private String mySound;      
+  private String myName;  
+  public NamedCow(String type, String name, String sound) {         
+    myType = type;  
+    myName = name;
+    mySound = sound;
+  }     
+  public NamedCow() {         
+    myType = "unknown";         
+    mySound = "unknown";
+  }      
+  public String getSound() {
+    return mySound;
+  }   
+  public String getName() {
+    return myName;
+  } 
+  public String getType() {
+    return myType;
+  }
 }
